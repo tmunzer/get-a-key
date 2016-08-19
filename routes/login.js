8f1bb6d3ce698410dc2ed5524321b14e5304e172
tmunzer <tmunzer@aerohive.com>
2016-08-19 08:15:15 +0100

    change file structure

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
 var passport = require('passport');
 var SamlStrategy = require('passport-saml').Strategy;
 
+var adfsOptions = require("./../passport/config.js").adfsOptions;
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -14,20 +14,7 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new SamlStrategy(
-  {
-    entryPoint: 'https://dc.ah-lab.fr/adfs/ls/',
-    issuer: 'get-a-key.ah-lab.fr',
-    callbackUrl: 'https://get-a-key.ah-lab.fr/adfs/postResponse',
-    privateCert: fs.readFileSync('./../certs/get_a_key.ah_lab.fr.key', 'utf-8'),
-    cert: fs.readFileSync('./../certs/get_a_key.ah_lab.fr.cert', 'utf-8'),
-  // other authn contexts are available e.g. windows single sign-on
-    authnContext: 'http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/password',
-  // not sure if this is necessary?
-    acceptedClockSkewMs: -1,
-    identifierFormat: null,
-  // this is configured under the Advanced tab in AD FS relying party
-    signatureAlgorithm: 'sha256'
-  },
+  adfsOptions,
   function(profile, done) {
     return done(null,
       {

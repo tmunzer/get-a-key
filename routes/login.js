433294b9852ec0dc0c6f376b59ae93bacf5759b1
tmunzer <tmunzer@aerohive.com>
2016-11-09 11:15:51 +0100

    fix azureAD logout

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var config = require("../config.js");
+var devAccount = require("../config.js").devAccount;
 
+var serverHostname = require("../config.js").appServer.vhost;
 var Account = require("../bin/models/account");
 var Customization = require("../bin/models/customization");
 
@@ -45,7 +46,7 @@ router.get("/login/:account_id/", getAccount, function (req, res) {
     else if (req.session.account.adfs) method = "/adfs/" + req.params.account_id + "/login";
     res.render("login", {
         title: 'Get a Key!',
-        oauthUrl: "https://cloud.aerohive.com/thirdpartylogin?client_id=" + config.devAccount.clientID + "&redirect_uri=" + config.devAccount.redirectUrl,
+        oauthUrl: "https://cloud.aerohive.com/thirdpartylogin?client_id=" + devAccount.clientID + "&redirect_uri=" + devAccount.redirectUrl,
         method: method,
         custom: req.custom
     });
@@ -58,13 +59,13 @@ router.get("/login/:account_id/callback", function (req, res) {
 router.get("/login", function (req, res) {
     res.render("login", {
         title: 'Get a Key!',
-        oauthUrl: "https://cloud.aerohive.com/thirdpartylogin?client_id=" + config.devAccount.clientID + "&redirect_uri=" + config.devAccount.redirectUrl,
+        oauthUrl: "https://cloud.aerohive.com/thirdpartylogin?client_id=" + devAccount.clientID + "&redirect_uri=" + devAccount.redirectUrl,
         method: null
     });
 })
 router.get("/logout/", function (req, res) {
     if (req.session.account.azureAd) {
-        res.redirect("https://login.windows.net/" + req.session.account.azureAd.tenant + "/oauth2/logout?post_logout_redirect_uri=" + req.session.account.azureAd.logoutURL);
+        res.redirect("https://login.windows.net/" + req.session.account.azureAd.tenant + "/oauth2/logout?post_logout_redirect_uri=https://" + serverHostname + "/login/" + account._id + "/");
     } else res.redirect("/");
     req.logout();
     req.session.destroy();

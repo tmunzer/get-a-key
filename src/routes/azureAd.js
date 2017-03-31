ccca5fe980b49aa10605316cf9892f81c9a25a18
Thomas Munzer <tmunzer@aerohive.com>
2017-03-31 15:59:21 +0200

    fix logging issue

--- src/routes/azureAd.js
+++ src/routes/azureAd.js
@@ -15,14 +15,14 @@ router.get('/:account_id/callback', getAzureAdAccount,
     function (req, res) {
         if (req.session.passport.user.email) req.session.email = req.session.passport.user.email;
         else req.session.email = req.session.passport.user.upn;
-        console.info("\x1b[32minfo\x1b[0m:", 'User ' + req.session.passport.user.upn + ' logged in');
+        console.info("\x1b[32minfo\x1b[0m:", 'User ' + req.session.email + ' logged in');
         res.redirect('/web-app/');
     }
 );
 
 /* Handle Logout */
 router.get('/:account_id/logout/', function (req, res) {
-    console.log("\x1b[32minfo\x1b[0m:", "User " + req.session.passport.user.upn + " is now logged out.");
+    console.log("\x1b[32minfo\x1b[0m:", "User " + req.session.email + " is now logged out.");
     req.logout();
     req.session.destroy();
     res.redirect('/login/:account_id/');

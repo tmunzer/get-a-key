142800e055e6717e45c4e4920b148b37b8e05e1c
Thomas Munzer <tmunzer@aerohive.com>
2017-04-28 08:36:41 +0200

    typo correction

--- src/public/i18n/login/fr.js
+++ src/public/i18n/login/fr.js
@@ -3,7 +3,7 @@ var fr = {
     settings: "Paramètres",
     login: {
         title: "Bienvenue sur le service Get-a-Key!",
-        instruction: "Pour obtenir votre clé personnelle pour vous connecter au réseau, veuillez cliquer sur le bouton ci-desous pour vous identify avec votre compte de domaine",
+        instruction: "Pour obtenir votre clé personnelle pour vous connecter au réseau, veuillez cliquer sur le bouton ci-dessous pour vous identifier avec votre compte de domaine",
         connect: "S'identifier"
     }
 }
